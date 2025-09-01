deff36bd8bf8ea451b9d2ec26304268abfd1ba7b
hugoalh <hugoalh@gmail.com>
2025-09-01 17:18:38 +0800

    Update test

--- mod.test.ts
+++ mod.test.ts
@@ -4,57 +4,58 @@ import {
 	isStringASCIIControl,
 	isStringASCIIPrintable
 } from "./mod.ts";
-const sample1 = "日本語";
-const sample2 = "👀";
-const sample3 = "Hello, world!";
-const sample4 = "";
-const sample5 = `Wisi sed et at vero eos nostrud volutpat sed stet dignissim sit sanctus in eros.
+interface TesterExpects {
+	all: boolean;
+	control: boolean;
+	printable: boolean;
+}
+async function tester(t: Deno.TestContext, item: string, expects: TesterExpects): Promise<void> {
+	await t.step("All", () => {
+		deepStrictEqual(isStringASCII(item), expects.all);
+	});
+	await t.step("Control", () => {
+		deepStrictEqual(isStringASCIIControl(item), expects.control);
+	});
+	await t.step("Printable", () => {
+		deepStrictEqual(isStringASCIIPrintable(item), expects.printable);
+	});
+}
+Deno.test("1", { permissions: "none" }, async (t) => {
+	await tester(t, "日本語", {
+		all: false,
+		control: false,
+		printable: false
+	});
+});
+Deno.test("2", { permissions: "none" }, async (t) => {
+	await tester(t, "👀", {
+		all: false,
+		control: false,
+		printable: false
+	});
+});
+Deno.test("3", { permissions: "none" }, async (t) => {
+	await tester(t, "Hello, world!", {
+		all: true,
+		control: false,
+		printable: true
+	});
+});
+Deno.test("4", { permissions: "none" }, async (t) => {
+	await tester(t, "", {
+		all: true,
+		control: true,
+		printable: true
+	});
+});
+Deno.test("5", { permissions: "none" }, async (t) => {
+	await tester(t, `Wisi sed et at vero eos nostrud volutpat sed stet dignissim sit sanctus in eros.
 Et laoreet odio sanctus ea.
 Sea in dolores diam tincidunt labore sea stet vero dolor ut est.
 At aliquyam diam facilisis lorem et takimata et volutpat eros erat ipsum velit labore sed ea illum.
-Dolor lorem sed et volutpat exerci gubergren gubergren tempor quis ea eirmod eos ut dolor autem ipsum accumsan.`;
-Deno.test("1 All", { permissions: "none" }, () => {
-	deepStrictEqual(isStringASCII(sample1), false);
-});
-Deno.test("1 Control", { permissions: "none" }, () => {
-	deepStrictEqual(isStringASCIIControl(sample1), false);
-});
-Deno.test("1 Printable", { permissions: "none" }, () => {
-	deepStrictEqual(isStringASCIIPrintable(sample1), false);
-});
-Deno.test("2 All", { permissions: "none" }, () => {
-	deepStrictEqual(isStringASCII(sample2), false);
-});
-Deno.test("2 Control", { permissions: "none" }, () => {
-	deepStrictEqual(isStringASCIIControl(sample2), false);
-});
-Deno.test("2 Printable", { permissions: "none" }, () => {
-	deepStrictEqual(isStringASCIIPrintable(sample2), false);
-});
-Deno.test("3 All", { permissions: "none" }, () => {
-	deepStrictEqual(isStringASCII(sample3), true);
-});
-Deno.test("3 Control", { permissions: "none" }, () => {
-	deepStrictEqual(isStringASCIIControl(sample3), false);
-});
-Deno.test("3 Printable", { permissions: "none" }, () => {
-	deepStrictEqual(isStringASCIIPrintable(sample3), true);
-});
-Deno.test("4 All", { permissions: "none" }, () => {
-	deepStrictEqual(isStringASCII(sample4), true);
-});
-Deno.test("4 Control", { permissions: "none" }, () => {
-	deepStrictEqual(isStringASCIIControl(sample4), true);
-});
-Deno.test("4 Printable", { permissions: "none" }, () => {
-	deepStrictEqual(isStringASCIIPrintable(sample4), true);
-});
-Deno.test("5 All", { permissions: "none" }, () => {
-	deepStrictEqual(isStringASCII(sample5), true);
-});
-Deno.test("5 Control", { permissions: "none" }, () => {
-	deepStrictEqual(isStringASCIIControl(sample5), false);
-});
-Deno.test("5 Printable", { permissions: "none" }, () => {
-	deepStrictEqual(isStringASCIIPrintable(sample5), false);
+Dolor lorem sed et volutpat exerci gubergren gubergren tempor quis ea eirmod eos ut dolor autem ipsum accumsan.`, {
+		all: true,
+		control: false,
+		printable: false
+	});
 });
