c95258122b3942a7395d52959bc1c05015516065
hugoalh <hugoalh@gmail.com>
2025-09-15 18:23:05 +0800

    Update resources

---
+++ .github-npm.build.ts
@@ -0,0 +1,44 @@
+import {
+	getMetadataFromConfig,
+	invokeDenoNodeJSTransformer
+} from "DNT";
+const configJSR = await getMetadataFromConfig("jsr.jsonc");
+await invokeDenoNodeJSTransformer({
+	copyAssets: [
+		"LICENSE.md",
+		"README.md"
+	],
+	entrypoints: configJSR.getExports(),
+	fixInjectedImports: true,
+	generateDeclarationMap: true,
+	metadata: {
+		name: configJSR.getName(),
+		version: configJSR.getVersion(),
+		description: "A module to determine whether the string is ASCII.",
+		keywords: [
+			"ascii",
+			"is",
+			"string"
+		],
+		homepage: "https://github.com/hugoalh/is-string-ascii-es#readme",
+		bugs: {
+			url: "https://github.com/hugoalh/is-string-ascii-es/issues"
+		},
+		license: "MIT",
+		author: "hugoalh",
+		repository: {
+			type: "git",
+			url: "git+https://github.com/hugoalh/is-string-ascii-es.git"
+		},
+		scripts: {
+		},
+		engines: {
+		},
+		private: false,
+		publishConfig: {
+			access: "public"
+		}
+	},
+	outputDirectory: "dist/github-npm",
+	outputDirectoryPreEmpty: true
+});
